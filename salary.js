9e71d584a0b3b961fdc49e23d346b3b6136a5e0a
hermo100 <orangiherman@gmail.com>
2023-12-06 11:45:16 +0300

    update one

--- salary.js
+++ salary.js
@@ -1,108 +1,118 @@
-function calc_gross(basic_salary, benefits) {
-    let gross_salary = basic_salary + benefits;
-    return gross_salary;
+let my_form = document.getElementById("myform");
+my_form.addEventListener("submit", function (e) {
+  //Prevent the webpage from refreshing
+  e.preventDefault();
+  //Check if inputs have values
+  let basic_salary = Number(document.getElementById("basic_salary").value);
+ // console.log(basic_salary);
+  let benefits = Number(document.getElementById("benefits").value);
+ // console.log(benefits);
+  if (basic_salary && benefits) {
+    let gross_salary = calc_gross(basic_salary ,benefits);
+    //console.log(gross_salary);
+    document.getElementById("gross_salary").innerText = gross_salary;
+    let nhif = calc_nhif(gross_salary);
+    document.getElementById("nhif").innerText = nhif;
+    let nssf=calc_nssf(gross_salary, rate = 0.06);
+    document.getElementById("nssf").innerText=nssf
+    let nhdf=calc_nhdf(gross_salary, rate = 0.015);
+    document.getElementById("nhdf").innerText=nhdf
+    let taxableincome=calc_taxableincome(gross_salary, nssf, nhdf);
+    document.getElementById("taxableincome").innerText=taxableincome
+    let payee=calc_payee(taxableincome);
+    document.getElementById("payee").innerText=payee
+    let netsalary=calc_netsalary(gross_salary,nhif, nhdf, nssf, payee);
+    document.getElementById("netsalary").innerText=netsalary
+  } else {
+    alert("insert all fields");
   }
-  let gross_salary = calc_gross();
-  console.log(gross_salary);
+});
 
-  let my_form = document.getElementById("gross");
-  let table=document.getElementById("table");
-  my_form.addEventListener("submit", function (e) {
-    //Prevent the webpage from refreshing
-    e.preventDefault();
-    //Check if inputs have values
-    let basic_salary = document.getElementById("basic salary").value;
-    let benefits = document.getElementById("benefits").value;
-    if (basic_salary.length === 0 || benefits.length === 0) {
-      alert("Ensure all Fields are set")
-      table.reset();
-        
-    } else {
-      let final_gross = calc_gross(Number(basic_salary), Number(benefits));
-      document.getElementById("subs").innerText = final_gross+' '+'Ksh';
-      my_form.reset();
-    }
-    function calc_nhif(gr) {
-      let final_gross = calc_gross(Number(basic_salary), Number(benefits));
-      
-      if (final_gross <= 5999) {
-        nhifContribution = 150;
-      } else if (final_gross >= 6000 && final_gross <= 7999) {
-        nhifContribution = 300;
-      } else if (final_gross >= 8000 && final_gross <= 11999) {
-        nhifContribution = 400;
-      } else if (final_gross >= 12000 && final_gross <= 14999) {
-        nhifContribution = 500;
-      } else if (final_gross >= 15000 && final_gross <= 19999) {
-        nhifContribution = 600;
-      } else if (final_gross >= 20000 && final_gross <= 24999) {
-        nhifContribution = 750;
-      } else if (final_gross >= 25000 && final_gross <= 29999) {
-        nhifContribution = 850;
-      } else if (final_gross >= 30000 && final_gross <= 34999) {
-        nhifContribution = 900;
-      } else if (final_gross >= 35000 && final_gross <= 39999) {
-        nhifContribution = 950;
-      } else if (final_gross >= 40000 && final_gross <= 44999) {
-        nhifContribution = 1000;
-      } else if (final_gross >= 45000 && final_gross <= 49999) {
-        nhifContribution = 1100;
-      } else if (final_gross >= 50000 && final_gross <= 59999) {
-        nhifContribution = 1200;
-      } else if (final_gross >= 60000 && final_gross <= 69999) {
-        nhifContribution = 1300;
-      } else if (final_gross >= 70000 && final_gross <= 79999) {
-        nhifContribution = 1400;
-      } else if (final_gross >= 80000 && final_gross <= 89999) {
-        nhifContribution = 1500;
-      } else if (final_gross >= 90000 && final_gross <= 99999) {
-        nhifContribution = 1600;
-      } else {
-        nhifContribution = 1700;
-      }
-      return nhifContribution;
-    }
-    calc_nhif();
-    let nhif = calc_nhif();
-  
-    
-    let final_gross = calc_gross(Number(basic_salary), Number(benefits));
-    let nssfRate = 0.06;
-    let maxContribution = 18000;
-    let nssfContribution = final_gross * nssfRate;
-    if(final_gross>18000){nssfContribution=maxContribution*0.06}
-   else{nssfContribution=final_gross*0.06}
-
-    let nhdfRate = 0.015;
-    let nhdfContribution = final_gross * nhdfRate;
-    let taxable_income=final_gross-(nssfContribution+nhdfContribution);
-    
-    
-    let payee=0;
-    let relief=2400;
-if (taxable_income <=24000) {
-    payee = 0;
-} else if (taxable_income>24000&&taxable_income <= 32333) {
-    payee = ((24000*0.1)+((taxable_income - 24000) * 0.25))-relief;
-} 
-else {
-    payee =((24000*0.1)+(8333*0.25)+((taxable_income - 32333) * 0.3))-2400;
+function calc_gross(a, b) {
+  let gross_salary = a + b;
+  return gross_salary;
 }
-let netSalary = final_gross - (nhifContribution + nssfContribution + nhdfContribution + payee);
 
+function calc_nhif(gross_salary) {
+  let nhif_contribution = 0;
+  if (gross_salary <= 5999) {
+    nhif_contribution = 150;
+  } else if (gross_salary >= 6000 && gross_salary <= 7999) {
+    nhif_contribution = 300;
+  } else if (gross_salary >= 8000 && gross_salary <= 11999) {
+    nhif_contribution = 400;
+  } else if (gross_salary >= 12000 && gross_salary <= 14999) {
+    nhif_contribution = 500;
+  } else if (gross_salary >= 15000 && gross_salary <= 19999) {
+    nhif_contribution = 600;
+  } else if (gross_salary >= 20000 && gross_salary <= 24999) {
+    nhif_contribution = 750;
+  } else if (gross_salary >= 25000 && gross_salary <= 29999) {
+    nhif_contribution = 850;
+  } else if (gross_salary >= 30000 && gross_salary <= 34999) {
+    nhif_contribution = 900;
+  } else if (gross_salary >= 35000 && gross_salary <= 39999) {
+    nhif_contribution = 950;
+  } else if (gross_salary >= 40000 && gross_salary <= 44999) {
+    nhif_contribution = 1000;
+  } else if (gross_salary >= 45000 && gross_salary <= 49999) {
+    nhif_contribution = 1100;
+  } else if (gross_salary >= 50000 && gross_salary <= 59999) {
+    nhif_contribution = 1200;
+  } else if (gross_salary >= 60000 && gross_salary <= 69999) {
+    nhif_contribution = 1300;
+  } else if (gross_salary >= 70000 && gross_salary <= 79999) {
+    nhif_contribution = 1400;
+  } else if (gross_salary >= 80000 && gross_salary <= 89999) {
+    nhif_contribution = 1500;
+  } else if (gross_salary >= 90000 && gross_salary <= 99999) {
+    nhif_contribution = 1600;
+  } else {
+    nhif_contribution = 1700;
+  }
+  return nhif_contribution;
+}
+function calc_nssf(gross_salary, rate = 0.06) {
+  if (gross_salary < 18000 && gross_salary > 0) {
+    nssf = gross_salary * rate;
+  } else {
+    nssf = 18000 * rate;
+  }
+  return nssf;
+}
 
-      document.getElementById("subs").innerText = final_gross+' '+'Ksh';
-    document.getElementById("nhif").innerText = nhif+' '+'Ksh';
-    document.getElementById("nhdf").innerText =
-      Number(nhdfContribution)+' '+'Ksh';
-      document.getElementById("nssf").innerText =
-       nssfContribution+' '+'Ksh';
-      document.getElementById("tax").innerText =
-      taxable_income+' '+'Ksh';
-      document.getElementById("payee").innerText =
-      Math.round(payee)+' '+'Ksh';
-      document.getElementById("net salary").innerText =
-       netSalary+'  '+'Ksh';
-      
-      
-  });
+function calc_nhdf(gross_salary, rate = 0.015) {
+  let nhdf = gross_salary * rate;
+
+  return nhdf;
+}
+
+function calc_taxableincome(gross_salary, nssf, nhdf) {
+  let taxableincome = gross_salary - (nssf + nhdf);
+
+  return taxableincome;
+}
+
+function calc_payee(taxableincome) {
+  if (taxableincome > 0 && taxableincome <= 24000) {
+     grosspayee = 0;
+    
+  } else if (taxableincome > 24000 && taxableincome <= 32333) {
+    payee = (taxableincome - 24000) * 0.25;
+    grosspayee = 24000 * 0.1 + payee - 2400;
+  }  else if (taxableincome > 24000 && taxableincome <= 32333) {
+    payee = (taxableincome - 24000) * 0.25;
+    grosspayee = 24000 * 0.1 + payee - 2400;
+  } 
+  else {
+    payee = (taxableincome - 32333) * 0.3;
+    grosspayee = 2400 + 8333 * 0.25 + payee - 2400;
+  }
+  return grosspayee;
+}
+
+function calc_netsalary(a, b, c, d,e) {
+  let netsalary = a - (b + c + d + e);
+
+  return netsalary;
+}
